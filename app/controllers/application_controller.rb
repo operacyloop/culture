9e67095fcae55313d8457c67e2b53a141dedc559
operacy <age.ef.ess@gmail.com>
2020-02-29 10:10:57 -0500

    example form added but no functionality yet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,22 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  # params = {
+  #   :name => "Johnson" //This is a String and NOT an Integer
+  # }
+
+  # get "/hello/:name" do
+  #   @user_name = params[:name]
+  #   #@user_name = "Barry"
+  #   "Hello #{@user_name}!"
+  # end
+
+  get "/multiply/:num1/:num2" do 
+    "#{params[:num1].to_i*params[:num2].to_i}"
+  end
+
   get "/" do
+    @posts = ["Family", "Entrprenuerial", "Structured", "Traditional", "Team-oriented", "Performance"]
     erb :welcome
   end
 
